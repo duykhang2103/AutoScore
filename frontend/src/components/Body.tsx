77e772d28fb052a8b14ab0366fa407ace97ef45f
DK <duykhang2103@gmail.com>
2024-06-06 14:28:22 +0700

    create api on fe

--- frontend/src/components/Body.tsx
+++ frontend/src/components/Body.tsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import InputForm from "./InputForm";
 import GradeContainer from "./GradeContainer";
 
 export default function Body() {
+  const [grade, setGrade] = useState<number>(0);
+
   return (
     <div className="flex flex-row">
-      <InputForm />
-      <GradeContainer />
+      <InputForm setGrade={setGrade} />
+      <GradeContainer grade={grade} />
     </div>
   );
 }
--- frontend/src/components/GradeContainer.tsx
+++ frontend/src/components/GradeContainer.tsx
@@ -1,25 +1,13 @@
 import React from "react";
 import { toVulgar } from "vulgar-fractions";
 
-export default function GradeContainer() {
+export default function GradeContainer({ grade }: { grade: number }) {
   return (
-    <div className="flex flex-col bg-cyan-200 m-[10px] rounded-md h-[550px]">
+    <div className="flex flex-col bg-cyan-200 m-[10px] rounded-md h-[550px] w-[400px]">
       <div className="w-[70px] h-[70px] rounded-full bg-green-300 text-center text-5xl pt-2 m-2">
-        {toVulgar(1 / 6)}
+        {toVulgar(grade / 6)}
       </div>
-      <div className="p-2 w-full h-[450px] text-wrap overflow-auto text-justify">
-        The essay demonstrates adequate mastery, although it will have lapses in
-        quality. A typical essay develops a point of view on the issue and
-        demonstrates competent critical thinking; the essay using adequate
-        examples, reasons, and other evidence taken from the source text(s) to
-        support its position; the essay is generally organized and focused,
-        demonstrating some coherence and progression of ideas exhibits adequate;
-        the essay may demonstrate inconsistent facility in the use of language,
-        using generally appropriate vocabulary demonstrates some variety in
-        sentence structure; the essay may have some errors in grammar, usage,
-        and mechanics.he essay demonstrates adequate mastery, although it will
-        have lapses in quality.
-      </div>
+      <div className="p-2 w-full h-[450px] text-wrap overflow-auto text-justify"></div>
     </div>
   );
 }
